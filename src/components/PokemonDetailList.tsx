63abe34a2910a01f1c07fd3d4aec5f1015a623b3
Hosh <235524154+fried-hosh@users.noreply.github.com>
2026-08-03 19:32:35 +0900

    feat: ポケモン詳細画面の基本レイアウトを追加 種族値パネルの作成

--- src/components/PokemonDetailList.tsx
+++ src/components/PokemonDetailList.tsx
@@ -1,5 +1,100 @@
-const PokemonDetailList = () => {
-  return <p></p>;
+import { type PokemonDetails } from "../api/getPokemonDetails";
+
+type Props = {
+  details: PokemonDetails;
+};
+
+const PokemonDetailList = ({ details }: Props) => {
+  const totalStats = details.stats
+    .map((stat) => stat.baseStat)
+    .reduce((acc, cur) => {
+      return acc + cur;
+    }, 0);
+
+  // パネルの共通レイアウト
+  const panelClassName = "p-5 rounded-2xl border text-left shadow-lg border-[var(--border)] bg-[var(--code-bg)]";
+
+  const MAX_BASE_STAT = 255;
+
+  return (
+    <div className="grid grid-cols-1 gap-4 p-4 lg:grid-cols-2 ">
+      {/* 基本情報 - 名前、画像、タイプ */}
+      <section className={panelClassName}>
+        <div className="grid grid-cols-2 items-center gap-4">
+          {details.imageUrl !== null && <img className="mx-auto w-full max-w-3xs" src={details.imageUrl} alt={details.name} />}
+          <div>
+            <h2>{details.name}</h2>
+            <ul className="flex gap-2">
+              {details.types.map((type) => (
+                <li key={type}>{type}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      {/* 進化チェーン */}
+      <section className={panelClassName}>
+        <h2>進化表</h2>
+        <ul>
+          {details.evolutions.map((evo) => (
+            <li key={evo.name}>{evo.name}</li>
+            // 各画像
+          ))}
+        </ul>
+      </section>
+
+      {/* 特性 */}
+      <section className={panelClassName}>
+        <h2>特性</h2>
+        <ul>
+          {details.abilities.map((ability) => (
+            <li key={ability.name}>
+              {ability.isHidden && <p>隠れ特性</p>}
+              {ability.name}
+            </li>
+            // 説明文
+          ))}
+        </ul>
+      </section>
+
+      {/* タイプ相性 */}
+      <section className={panelClassName}>
+        <h2>タイプ相性</h2>
+      </section>
+
+      {/* 種族値 */}
+      <section className={panelClassName}>
+        <div className="flex justify-between items-center">
+          <h2>種族値</h2>
+          <span>
+            合計
+            <strong> {totalStats}</strong>
+          </span>
+        </div>
+        <ul>
+          {details.stats.map((stat) => {
+            const percentage = (stat.baseStat / MAX_BASE_STAT) * 100;
+            return (
+              <li key={stat.name} className="flex justify-between items-center">
+                <span className="w-36 shrink-0">{stat.name}</span>
+                {/* ゲージ */}
+                <div className="h-2 flex-1 rounded-full bg-slate-200">
+                  <div className="h-full rounded-full bg-orange-400" style={{ width: `${percentage}%` }} />
+                </div>
+                <span className="w-10 shrink-0 text-right tabular-nums">{stat.baseStat}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+
+      {/* 覚える技 */}
+      <section className={panelClassName}>
+        <h2>覚える技</h2>
+      </section>
+    </div>
+  );
 };
 
 export default PokemonDetailList;
--- src/pages/PokemonDetailPage.tsx
+++ src/pages/PokemonDetailPage.tsx
@@ -1,11 +1,19 @@
 import { useParams } from "react-router-dom";
 import { usePokemonDetails } from "../hooks/usePokemonDetails";
+import PokemonDetailList from "../components/PokemonDetailList";
 
 const PokemonDetailPage = () => {
   const { pokemonName } = useParams();
-  usePokemonDetails(pokemonName);
+  const { data, isLoading, isError, isSuccess } = usePokemonDetails(pokemonName);
 
-  return <p>{pokemonName}</p>;
+  return (
+    <div>
+      {isLoading && <p>読み込み中...</p>}
+      {isError && <p className="text-red-200">取得に失敗しました</p>}
+
+      {isSuccess && <PokemonDetailList details={data} />}
+    </div>
+  );
 };
 
 export default PokemonDetailPage;
